273b06f0b2b3c9a78fb4a201cd41bd3d1c38193d
L-Hejer <laouani.h@gmail.com>
2020-07-01 12:29:10 +0100

    first commit

---
+++ build/precache-manifest.a9cc898af867d64154dc2c37818744b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4d046cf5bb29b124e5da21275fd72f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0ed8a300895bd3838b0",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8db9b8e6ffb732914c2b",
+    "url": "/static/css/main.8d0c277a.chunk.css"
+  },
+  {
+    "revision": "e0ed8a300895bd3838b0",
+    "url": "/static/js/2.9a4a9ae7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9a4a9ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8db9b8e6ffb732914c2b",
+    "url": "/static/js/main.dbb01337.chunk.js"
+  },
+  {
+    "revision": "2aadb0774d2fa5bd2379",
+    "url": "/static/js/runtime-main.c76a528e.js"
+  }
+]);
---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+// const cors = require('cors');
+const photosRouter = require('./routers/photos');
+require('./db/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3300;
+
+app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(photosRouter);
+
+// add middleware
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
